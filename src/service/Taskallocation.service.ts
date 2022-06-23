e2e6f0348ace6c619f9406d270acf3972da44a26
Nandalalainfotech <nandalalainfotech.com>
2022-06-23 20:17:23 +0530

    Backend updated done by Moorthy

--- src/service/Taskallocation.service.ts
+++ src/service/Taskallocation.service.ts
@@ -30,15 +30,20 @@ export class TaskallocationService {
     async createTaskAllocation(taskallocationDTO: TaskallocationDTO): Promise<Taskallocation001wb[]> {
         const file2 = await reader.readFile("./uploads/helloworld.xlsx");
         const sheet1 = reader.utils.sheet_to_json(file2.Sheets[file2.SheetNames[0]]);
+        
         let sheet = JSON.parse(JSON.stringify(sheet1).replace(/\s(?=\w+":)/g, ""));
+        
         let taskallocation001wbs: Taskallocation001wb[] = [];
         let reviewers: User001mb[] = [];
         reviewers = await this.userRepository.find({ relations: ["person", "role"], where: { roleid: 3 } });
         for (let i = 0; i < sheet.length; i++) {
+           
+            
+            
             const taskallocation001wb = new Taskallocation001wb();
             taskallocation001wb.curatorId = i + 1;
             taskallocation001wb.curatorName = sheet[i].CURATORNAME;
-            taskallocation001wb.cbatchNo = "A1";
+            taskallocation001wb.cbatchNo = sheet[i].CBATCHNUMBER;
             taskallocation001wb.curatorTanNo = sheet[i].TANNUMBER;
             taskallocation001wb.curatorAllocateDate = sheet[i].curatorAllocateDate;
             taskallocation001wb.insertUser = taskallocationDTO.insertUser;
@@ -47,7 +52,7 @@ export class TaskallocationService {
             let random = Math.floor(Math.random() * reviewers.length);
             taskallocation001wb.reviewerName = reviewers[random].username;
             taskallocation001wb.reviewerTanNo = sheet[i].TANNUMBER;
-            taskallocation001wb.rbatchNo = "";
+            taskallocation001wb.rbatchNo = sheet[i].RBATCHNUMBER;
             this.taskAllocateRepository.save(taskallocation001wb);
             taskallocation001wbs.push(taskallocation001wb);
         }
