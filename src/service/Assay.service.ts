3dba3783cccdf0b91a5c975a751bd46e78e77182
nandalalainfotech@gmail.com <Spartans!23>
2022-06-25 19:02:47 +0530

    Changes done by sekar

--- src/service/Assay.service.ts
+++ src/service/Assay.service.ts
@@ -57,40 +57,58 @@ export class AssayService {
             where: { insertUser: username }, relations: ["assayTypeSlno2", "toxiCitySlno2", "routeSlno2", "unitSlno2", "unitedSlno2", "ligandSlno2", "ligandSlno2.ligandVersionSlno2", "ligandSlno2.ligandTypeSlno2", "categorySlno2", "functionSlno2", "originalPrefixSlno2", "typeSlno2"]
         });
         for (let assay001wb of assay001wbs) {
-            assay001wb.administration = unescape(assay001wb.administration)
-            assay001wb.procedure = unescape(assay001wb.procedure)
-            assay001wb.ligandSvalue = unescape(assay001wb.ligandSvalue)
-            assay001wb.ligandHvalue = unescape(assay001wb.ligandHvalue)
-            assay001wb.ligandLvalue = unescape(assay001wb.ligandLvalue)
-            assay001wb.conditionMaterial = unescape(assay001wb.conditionMaterial)
-            assay001wb.conditionMaterialid = unescape(assay001wb.conditionMaterialid)
-            assay001wb.singleCondition = unescape(assay001wb.singleCondition)
-            assay001wb.highCondition = unescape(assay001wb.highCondition)
-            assay001wb.lowCondition = unescape(assay001wb.lowCondition)
-            assay001wb.dataLocator1 = unescape(assay001wb.dataLocator1)
-            assay001wb.dataLocator2 = unescape(assay001wb.dataLocator2)
-            assay001wb.dataLocator3 = unescape(assay001wb.dataLocator3)
-            assay001wb.parameter = unescape(assay001wb.parameter)
-            assay001wb.parameterDetail = unescape(assay001wb.parameterDetail)
-            assay001wb.singleValue = unescape(assay001wb.singleValue)
-            assay001wb.highEndValue = unescape(assay001wb.highEndValue)
-            assay001wb.lowEndValue = unescape(assay001wb.lowEndValue)
-            assay001wb.nonNumeric = unescape(assay001wb.nonNumeric)
-            assay001wb.remark = unescape(assay001wb.remark)
-            assay001wb.cell = unescape(assay001wb.cell)
-            assay001wb.cellDetail = unescape(assay001wb.cellDetail)
-            assay001wb.organ = unescape(assay001wb.organ)
-            assay001wb.organDetail = unescape(assay001wb.organDetail)
-            assay001wb.species = unescape(assay001wb.species)
-            assay001wb.speciesDetail = unescape(assay001wb.speciesDetail)
-            assay001wb.ageGroup = unescape(assay001wb.ageGroup)
-            assay001wb.targetVersion = unescape(assay001wb.targetVersion)
-            assay001wb.collectionId1 = unescape(assay001wb.collectionId1)
-            assay001wb.original = unescape(assay001wb.original)
-            assay001wb.acronym = unescape(assay001wb.acronym)
-            assay001wb.organism = unescape(assay001wb.organism)
-            assay001wb.variant = unescape(assay001wb.variant)
+            assay001wb.administration = unescape(assay001wb.administration);
+            assay001wb.procedure = unescape(assay001wb.procedure);
+            assay001wb.ligandSvalue = unescape(assay001wb.ligandSvalue);
+            assay001wb.ligandHvalue = unescape(assay001wb.ligandHvalue);
+            assay001wb.ligandLvalue = unescape(assay001wb.ligandLvalue);
+            assay001wb.conditionMaterial = unescape(assay001wb.conditionMaterial);
+            assay001wb.conditionMaterialid = unescape(assay001wb.conditionMaterialid);
+            assay001wb.singleCondition = unescape(assay001wb.singleCondition);
+            assay001wb.highCondition = unescape(assay001wb.highCondition);
+            assay001wb.lowCondition = unescape(assay001wb.lowCondition);
+            assay001wb.dataLocator1 = unescape(assay001wb.dataLocator1);
+            assay001wb.dataLocator2 = unescape(assay001wb.dataLocator2);
+            assay001wb.dataLocator3 = unescape(assay001wb.dataLocator3);
+            assay001wb.parameter = unescape(assay001wb.parameter);
+            assay001wb.parameterDetail = unescape(assay001wb.parameterDetail);
+            assay001wb.singleValue = unescape(assay001wb.singleValue);
+            assay001wb.highEndValue = unescape(assay001wb.highEndValue);
+            assay001wb.lowEndValue = unescape(assay001wb.lowEndValue);
+            assay001wb.nonNumeric = unescape(assay001wb.nonNumeric);
+            assay001wb.remark = unescape(assay001wb.remark);
+            assay001wb.cell = unescape(assay001wb.cell);
+            assay001wb.cellDetail = unescape(assay001wb.cellDetail);
+            assay001wb.organ = unescape(assay001wb.organ);
+            assay001wb.organDetail = unescape(assay001wb.organDetail);
+            assay001wb.species = unescape(assay001wb.species);
+            assay001wb.speciesDetail = unescape(assay001wb.speciesDetail);
+            assay001wb.ageGroup = unescape(assay001wb.ageGroup);
+            assay001wb.targetVersion = unescape(assay001wb.targetVersion);
+            assay001wb.collectionId1 = unescape(assay001wb.collectionId1);
+            assay001wb.original = unescape(assay001wb.original);
+            assay001wb.acronym = unescape(assay001wb.acronym);
+            assay001wb.organism = unescape(assay001wb.organism);
+            assay001wb.variant = unescape(assay001wb.variant);
 
+            assay001wb.unit = unescape(assay001wb.unit);
+            assay001wb.units = unescape(assay001wb.units);
+            assay001wb.collectionId = unescape(assay001wb.collectionId);
+            assay001wb.conditionType = unescape(assay001wb.conditionType);
+            assay001wb.highLowUnit = unescape(assay001wb.highLowUnit);
+            assay001wb.status = unescape(assay001wb.status);
+
+            assay001wb.ligandSlno2.tanNumber = unescape(assay001wb.ligandSlno2.tanNumber);
+            assay001wb.ligandSlno2.collection = unescape(assay001wb.ligandSlno2.collection);
+            assay001wb.ligandSlno2.ligandDetail = unescape(assay001wb.ligandSlno2.ligandDetail);
+            assay001wb.ligandSlno2.identifier1 = unescape(assay001wb.ligandSlno2.identifier1);
+            assay001wb.ligandSlno2.identifier2 = unescape(assay001wb.ligandSlno2.identifier2);
+            assay001wb.ligandSlno2.identifier3 = unescape(assay001wb.ligandSlno2.identifier3);
+            assay001wb.ligandSlno2.collectionId = unescape(assay001wb.ligandSlno2.collectionId);
+            assay001wb.ligandSlno2.locator = unescape(assay001wb.ligandSlno2.locator);
+            assay001wb.ligandSlno2.diseaseName1 = unescape(assay001wb.ligandSlno2.diseaseName1);
+            assay001wb.ligandSlno2.diseaseName2 = unescape(assay001wb.ligandSlno2.diseaseName2);
+            assay001wb.ligandSlno2.diseaseName3 = unescape(assay001wb.ligandSlno2.diseaseName3);
         }
         return assay001wbs;
 
