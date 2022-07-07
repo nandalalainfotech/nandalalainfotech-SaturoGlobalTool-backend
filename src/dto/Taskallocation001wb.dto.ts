a64d5138c02872fb442e7f7c6f48463c2b229b90
Nandalalainfotech <nandalalainfotech.com>
2022-07-07 16:51:20 +0530

    Backend Accept and Reject method changes done by Sekar and Moorthy.

--- src/dto/Taskallocation001wb.dto.ts
+++ src/dto/Taskallocation001wb.dto.ts
@@ -6,15 +6,18 @@ export class TaskallocationDTO {
     cbatchNo: string | null;
     curatorTanNo: string | null;
     curatorAllocateDate: Date | null;
+    status: string | null;
     reviewerName: string | null;
     rbatchNo: string | null;
     reviewerTanNo: string | null;
     reviewerAllocateDate: Date;
+    reviewerStatus: string | null;
     filename: string | null;
     insertUser: string;
     insertDatetime: Date;
     updatedUser: string | null;
     updatedDatetime: Date | null;
+    reviewerUpdatedDate: Date | null;
 
     setProperties(taskallocation001wb: Taskallocation001wb) {
         this.curatorId = taskallocation001wb.curatorId;
@@ -22,14 +25,17 @@ export class TaskallocationDTO {
         this.cbatchNo = taskallocation001wb.cbatchNo;
         this.curatorTanNo = taskallocation001wb.curatorTanNo;
         this.curatorAllocateDate = taskallocation001wb.curatorAllocateDate;
+        this.status = taskallocation001wb.status;
         this.reviewerName = taskallocation001wb.reviewerName;
         this.rbatchNo = taskallocation001wb.rbatchNo;
         this.reviewerTanNo = taskallocation001wb.reviewerTanNo;
         this.filename = taskallocation001wb.filename;
         this.reviewerAllocateDate = taskallocation001wb.reviewerAllocateDate;
+        this.reviewerStatus = taskallocation001wb.reviewerStatus;
         this.insertUser = taskallocation001wb.insertUser;
         this.insertDatetime = taskallocation001wb.insertDatetime;
         this.updatedUser = taskallocation001wb.updatedUser;
         this.updatedDatetime = taskallocation001wb.updatedDatetime;
+        this.reviewerUpdatedDate = taskallocation001wb.reviewerUpdatedDate;
     }
 }
